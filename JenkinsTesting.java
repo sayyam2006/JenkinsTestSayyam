a755e11ca382d345dabf1ce07b11e62b8e123370
sayyam2006 <sayyam2006@gmail.com>
2019-12-18 12:13:41 -0500

    Added Sayyam Jenkins

---
+++ JenkinsTesting.java
@@ -0,0 +1,17 @@
+package JenkinsTest;
+
+public class JenkinsTesting {
+	
+	public static void main(String[] args) {
+		//WebDriver driver = new ChromeDriver();
+		//driver.get("https://www.google.com");
+		//System.setProperty("webdriver.chrome.driver", "/chromedriver");
+		System.out.println("hello sayyam this is test");
+		
+		
+		//driver.close();
+
+
+}
+
+}
